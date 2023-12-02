65912e1167c097949ce6f179d69b10aacb253c55
FernandoA182 <ferndoarturo182@gmail.com>
2023-12-02 00:27:58 -0600

    Ejercicios completos

---
+++ 2- 0.1. RDD_pyspark-FernandoArevalo/0.1.  rdd_pyspark-multiplica_spark-FernandoArevalo.py
@@ -0,0 +1,23 @@
+# Alumno: Fernando Arturo Arevalo Perez
+# Numero de Alumno: 323018942
+# rdd_pyspark - multiplica_spark 
+
+import findspark
+findspark.init()
+from pyspark import SparkContext
+
+# Crea un contexto Spark
+sc = SparkContext("local", "EjemploRDD")
+#sc=  SparkContext(appName="MyAppName")
+
+# Crea un RDD a partir de una lista
+rdd = sc.parallelize([1, 2, 3, 4, 5])
+
+# Realiza una transformación: multiplica cada elemento por 2
+rdd_multiplicado = rdd.map(lambda x: x * 2)
+
+# Realiza una acción: muestra los resultados
+print(rdd_multiplicado.collect())
+#rdd_multiplicado.collect()
+# Detén el contexto Spark
+sc.stop()
